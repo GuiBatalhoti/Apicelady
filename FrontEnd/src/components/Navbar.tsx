da85c73c07ca02aa187a941f7785b664455e02ae
GuiBatalhoti <guibatalhoti@gmail.com>
2024-11-13 11:16:38 -0300

    Frontend - início navbar

---
+++ FrontEnd/src/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import { AppBar, Toolbar, IconButton, Stack, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useUser } from "../context/userContext";
+
+export default function Navbar() {
+
+    const navigate = useNavigate();
+    const { logout } = useUser();
+
+    const handleClick = (buttonName: string) => {
+        if (buttonName === "home") return () => navigate("/home/:userID");
+        if (buttonName === "sobre") return () => navigate("/sobre");
+        if (buttonName === "logout") return () => logout();
+    }
+
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton edge="start" color="inherit" aria-label="name" size="large">
+                    Apicelady
+                </IconButton>
+                <Stack direction="row" spacing={2}>
+                    <Button color="inherit" id="home-button" onClick={handleClick("home")}>Home</Button>
+                    <Button color="inherit" id="sobre-button" onClick={handleClick("sobre")}>Sobre</Button>
+                    <Button color="inherit" id="logout-button" onClick={handleClick("logout")}>Sair</Button>
+                </Stack>
+            </Toolbar>
+        </AppBar>
+    )
+}
