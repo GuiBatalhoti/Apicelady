43ce65db5cc25a84f046b57555457190db41663c
GuiBatalhoti <guibatalhoti@gmail.com>
2024-11-14 09:37:54 -0300

    Frontend - Adicionando os itens da página Home

--- FrontEnd/src/components/HomeItem.tsx
+++ FrontEnd/src/components/HomeItem.tsx
@@ -1,7 +1,17 @@
-export default function HomeItem() {    
+import { useNavigate } from 'react-router-dom';
+import { HomeItemProps } from '../types/HomeItemProps';
+import "../styles/HomeItem.css";
+import { Typography } from '@mui/material';
+
+export default function HomeItem({ icon, label, path }: HomeItemProps) {
+    const navigate = useNavigate();
+
     return (
-        <div>
-            <h1>Item</h1>
+        <div className="home-item" onClick={() => navigate(path)}>
+            {icon}
+            <Typography component="p" >
+                {label}
+            </Typography>
         </div>
     )
 }
--- FrontEnd/src/components/Navbar.tsx
+++ FrontEnd/src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { AppBar, Toolbar, IconButton, Stack, Button, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../context/userContext";
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 
 export default function Navbar() {
 
@@ -8,15 +9,19 @@ export default function Navbar() {
     const { user, logout } = useUser();
 
     const handleClick = (buttonName: string) => {
-        if (buttonName === "home") return () => navigate("/home/:userID");
+        if (buttonName === "home") return () => navigate("/home");
         if (buttonName === "sobre") return () => navigate("/sobre");
-        if (buttonName === "logout") return () => logout();
+        if (buttonName === "logout") return () => {
+            logout()
+            navigate("/login");
+        };
     }
 
     return (
         <AppBar position="static">
             <Toolbar>
                 <IconButton edge="start" color="inherit" aria-label="name" size="large">
+                    <AccountBalanceIcon style={{marginRight:"5px"}}/>
                     Apicelady
                 </IconButton>
                 <Stack direction="row" spacing={2}>
