12298e65bc902e63e1bca5b671b94b8f48a6efff
GuiBatalhoti <guibatalhoti@gmail.com>
2024-11-25 11:35:00 -0300

    Frontend - corrigindo a estrutura de dados das Salas

--- FrontEnd/src/types/DataStructures/Sala.ts
+++ FrontEnd/src/types/DataStructures/Sala.ts
@@ -1,8 +1,11 @@
 export interface Sala {
     docId: string,
     nome: string; //único
+    sigla: string;
+    descricao: string;
     numero: number; 
     predioNome: number;
+    deptoSigla: string;
     area: number;
     latitude: number;
     longitude: number;
