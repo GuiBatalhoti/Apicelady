62f6b41b12f8dcb43525772b9559d1dd8a3c644a
GuiBatalhoti <guibatalhoti@gmail.com>
2024-12-03 09:57:11 -0300

    Frontend - Separando o GenericTable e Row

--- FrontEnd/src/types/GenericTableProps.ts
+++ FrontEnd/src/types/GenericTableProps.ts
@@ -12,3 +12,12 @@ export interface GenericTableProps<DataType> {
     onDelete: (data: DataType) => void;
     onSelectRow: (item: DataType | null) => void;
 }
+
+export interface GenericTableRowProps<DataType> {
+    row: DataType;
+    columns: Column<DataType>[];
+    isSelected: boolean;
+    onSelect: () => void;
+    onEdit: (data: DataType) => void;
+    onDelete: (data: DataType) => void;
+}
