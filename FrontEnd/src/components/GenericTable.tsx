4977d2647eaa8aa65af1f938a2af3f38388b80b9
GuiBatalhoti <guibatalhoti@gmail.com>
2024-12-03 09:57:11 -0300

    Frontend - Separando o GenericTable e Row

--- FrontEnd/src/components/GenericTable.tsx
+++ FrontEnd/src/components/GenericTable.tsx
@@ -1,22 +1,11 @@
 import React, { useState } from 'react';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  TableSortLabel,
-} from '@mui/material';
+import { Table, TableContainer, TableHead, TableBody, TableRow, TableSortLabel, TableCell } from '@mui/material';
 import Paper from '@mui/material/Paper';
-import { IconButton, Radio } from '@mui/material';
 import { TableVirtuoso, TableComponents } from 'react-virtuoso';
 import { GenericTableProps } from '../types/GenericTableProps';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
-import '../styles/GenericTable.css';
+import GenericTableRow from './GenericTableRow';
 
-function GenericTable<DataType>({ columns, data, onEdit, onDelete, onSelectRow }: GenericTableProps<DataType>) {
+function GenericTable<DataType extends Record<string, any>>({ columns, data, onEdit, onDelete, onSelectRow }: GenericTableProps<DataType>) {
   const [selectedRow, setSelectedRow] = useState<DataType | null>(null);
   const [order, setOrder] = useState<'asc' | 'desc'>('asc');
   const [orderBy, setOrderBy] = useState<string | null>(null);
@@ -61,29 +50,13 @@ function GenericTable<DataType>({ columns, data, onEdit, onDelete, onSelectRow }
 
   const fixedHeaderContent = () => (
     <TableRow>
-      <TableCell
-        key="selection-header"
-        variant="head"
-        style={{ width: 50 }}
-        sx={{ backgroundColor: 'background.paper' }}
-      >
-        Select
-      </TableCell>
-      <TableCell
-        key="actions-header"
-        variant="head"
-        style={{ width: 80 }}
-        sx={{ backgroundColor: 'background.paper' }}
-      >
-        Actions
-      </TableCell>
+      <TableCell style={{ width: 50 }}>Select</TableCell>
+      <TableCell style={{ width: 80 }}>Actions</TableCell>
       {columns.map((column) => (
         <TableCell
           key={String(column.dataKey)}
-          variant="head"
           align={column.numeric ? 'right' : 'left'}
           style={{ width: column.width }}
-          sx={{ backgroundColor: 'background.paper' }}
         >
           <TableSortLabel
             active={orderBy === column.dataKey}
@@ -97,106 +70,22 @@ function GenericTable<DataType>({ columns, data, onEdit, onDelete, onSelectRow }
     </TableRow>
   );
 
-  const rowContent = (_index: number, row: DataType) => {
-    const [expandedRows, setExpandedRows] = useState<Record<number, boolean>>({});
-  
-    const toggleRow = (index: number) => {
-      setExpandedRows((prev) => ({
-        ...prev,
-        [index]: !prev[index],
-      }));
-    };
-  
-    const formatDate = (date: Date) =>{
-      return date.toLocaleDateString("pt-BR", {
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-      });
-    }
-  
-    return (
-      <React.Fragment>
-        <TableCell key="selection-cell" style={{ width: 20 }}>
-          <Radio
-            checked={row === selectedRow}
-            onChange={() => handleRowSelect(row)}
-            inputProps={{ 'aria-label': `select-row-${_index}` }}
-          />
-        </TableCell>
-        <TableCell key="actions-cell" style={{ width: 20 }}>
-          <IconButton onClick={() => onEdit(row)} aria-label="edit">
-            <EditIcon className="edit-icon" />
-          </IconButton>
-          <IconButton onClick={() => onDelete(row)} aria-label="delete">
-            <DeleteIcon className="delete-icon" />
-          </IconButton>
-        </TableCell>
-        {columns.map((column) => {
-          const value = row[column.dataKey];
-          const isArray = Array.isArray(value);
-  
-          return (
-            <TableCell key={String(column.dataKey)} align={column.numeric ? 'right' : 'left'}>
-              {isArray ? (
-                <>
-                  {/* Exibe o último elemento da lista */}
-                  <div>
-                    {Object.values(value[value.length - 1])
-                      .map((field) => {
-                        if (field instanceof Date) {
-                          return formatDate(field);
-                        }
-                        return String(field);
-                      })
-                      .join(" ")}
-                    <IconButton size="small" onClick={() => toggleRow(_index)}>
-                      {expandedRows[_index] ? '▲' : '▼'}
-                    </IconButton>
-                  </div>
-                  {/* Exibe o histórico completo caso esteja expandido */}
-                  {expandedRows[_index] && (
-                    <div style={{ marginTop: 8 }}>
-                      {value.slice(0, -1).map((item, idx) => (
-                        <div
-                          key={idx}
-                          style={{
-                            fontSize: "0.9em",
-                            color: "gray",
-                            display: "flex",
-                            gap: "8px",
-                          }}
-                        >
-                          {Object.values(item)
-                            .map((field) => {
-                              if (field instanceof Date) {
-                                return formatDate(field);
-                              }
-                              return String(field);
-                            })
-                            .join(" ")}
-                        </div>
-                      ))}
-                    </div>
-                  )}
-                </>
-              ) : (
-                String(value)
-              )}
-            </TableCell>
-          );
-        })}
-      </React.Fragment>
-    );
-  };  
-
   return (
     <Paper style={{ height: 400, width: '100%' }}>
       <TableVirtuoso
         data={sortedData}
         components={VirtuosoTableComponents}
         fixedHeaderContent={fixedHeaderContent}
-        itemContent={rowContent}
+        itemContent={(_index, row) => (
+          <GenericTableRow
+            row={row}
+            columns={columns}
+            isSelected={row === selectedRow}
+            onSelect={() => handleRowSelect(row)}
+            onEdit={onEdit}
+            onDelete={onDelete}
+          />
+        )}
       />
     </Paper>
   );
---
+++ FrontEnd/src/components/GenericTableRow.tsx
@@ -0,0 +1,99 @@
+import { useState } from 'react';
+import { TableCell, IconButton, Radio } from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { GenericTableRowProps } from '../types/GenericTableProps';
+import "../styles/GenericTable.css"
+
+const GenericTableRow = <DataType extends Record<string, any>,>({ row, columns, isSelected, onSelect, onEdit, onDelete, }: GenericTableRowProps<DataType>) => {
+  const [expandedRows, setExpandedRows] = useState<Record<number, boolean>>({});
+
+  const toggleRow = (index: number) => {
+    setExpandedRows((prev) => ({
+      ...prev,
+      [index]: !prev[index],
+    }));
+  };
+
+  const formatDate = (date: Date) => {
+    return date.toLocaleDateString('pt-BR', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+    });
+  };
+
+  return (
+    <>
+      <TableCell style={{ width: 15 }}>
+        <Radio
+          checked={isSelected}
+          onChange={onSelect}
+          inputProps={{ 'aria-label': 'select-row' }}
+        />
+      </TableCell>
+      <TableCell style={{ width: 15 }}>
+        <IconButton onClick={() => onEdit(row)} aria-label="edit">
+          <EditIcon className='edit-icon'/>
+        </IconButton>
+        <IconButton onClick={() => onDelete(row)} aria-label="delete">
+          <DeleteIcon className='delete-icon'/>
+        </IconButton>
+      </TableCell>
+      {columns.map((column) => {
+        const value = row[column.dataKey];
+        const isArray = Array.isArray(value);
+
+        return (
+          <TableCell key={String(column.dataKey)} align={column.numeric ? 'right' : 'left'}>
+            {isArray ? (
+              <>
+                <div>
+                  {Object.values(value[value.length - 1])
+                    .map((field) => {
+                      if (field instanceof Date) {
+                        return formatDate(field);
+                      }
+                      return String(field);
+                    })
+                    .join(' ')}
+                  <IconButton size="small" onClick={() => toggleRow(value.length)}>
+                    {expandedRows[value.length] ? '▲' : '▼'}
+                  </IconButton>
+                </div>
+                {expandedRows[value.length] && (
+                  <div style={{ marginTop: 8 }}>
+                    {value.slice(0, -1).map((item: Record<string, any>, idx: number) => (
+                      <div
+                        key={idx}
+                        style={{
+                          fontSize: '0.9em',
+                          color: 'gray',
+                          display: 'flex',
+                          gap: '8px',
+                        }}
+                      >
+                        {Object.values(item)
+                          .map((field: any) => {
+                            if (field instanceof Date) {
+                              return formatDate(field);
+                            }
+                            return String(field);
+                          })
+                          .join(' ')}
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </>
+            ) : (
+              String(value)
+            )}
+          </TableCell>
+        );
+      })}
+    </>
+  );
+};
+
+export default GenericTableRow;
