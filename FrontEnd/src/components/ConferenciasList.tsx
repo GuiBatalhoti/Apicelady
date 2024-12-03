58eff20e6b8aceb372905d45ae7ac94cc0ceed49
GuiBatalhoti <guibatalhoti@gmail.com>
2024-12-03 15:30:16 -0300

    Frontend - Conferências List

--- FrontEnd/src/components/ConferenciasList.tsx
+++ FrontEnd/src/components/ConferenciasList.tsx
@@ -2,69 +2,54 @@ import { useEffect, useState } from "react";
 import { Button, Typography } from "@mui/material";
 import MeetingRoom from '@mui/icons-material/MeetingRoom';
 import ComputerIcon from '@mui/icons-material/Computer';
-import ApartmentIcon from "@mui/icons-material/Apartment";
+import AddToQueueIcon from '@mui/icons-material/AddToQueue';
 import GenericTable from "./GenericTable";
 import GenericDialog from "./GenericDialog"; // Novo dialog genérico
-import ConfirmDialog from "./ConfirmDialog";
 import { Column } from "../types/GenericTableProps";
-import { Sala } from "../types/DataStructures/Sala";
-import { getAllFromCollection, deleteItem, createItem, updateItem } from "../config/firebase";
+import { getAllFromCollection, createItem } from "../config/firebase";
 import { DocumentData } from "firebase/firestore";
-import "../styles/Lists.css"; // Estilo alterado para salas
+import "../styles/Lists.css"; // Estilo alterado para conferencias
 import { useNavigate } from "react-router-dom";
-import { Departamento } from "../types/DataStructures/Departamento";
+import { Conferencia } from "../types/DataStructures/Conferencia";
+import { Sala } from "../types/DataStructures/Sala";
 
-export default function ConferenciasList() {
+export default function ConferenciaList() {
 
   const navigate = useNavigate();
 
-  const [salasList, setSalasList] = useState<Sala[]>([]);
-  const [selectedItem, setSelectedItem] = useState<Sala | null>(null);
-  const [selectedRow, setSelectedRow] = useState<Sala | null>(null);
+  const [conferenciasList, setConferenciasList] = useState<Conferencia[]>([]);
+  const [selectedItem, setSelectedItem] = useState<Conferencia | null>(null);
+  const [selectedRow, setSelectedRow] = useState<Conferencia | null>(null);
   const [dialogOpen, setDialogOpen] = useState(false);
-  const [confirmOpen, setConfirmOpen] = useState(false);
-  const [isEditing, setIsEditing] = useState(false); // Define se o dialog é para edição ou adição
+  const [salas, setSalas] = useState<Sala[]>([]);
 
-  const [departamentos, setDepartamentos] = useState<Departamento[]>([]);
-
-  const columns: Column<Sala>[] = [
-    { label: "Nome", dataKey: "nome", numeric: false, width: 150 },
-    { label: "Sigla", dataKey: "sigla", numeric: false, width: 40 },
-    { label: "Número", dataKey: "numero", numeric: true, width: 40 },
-    { label: "Departamento", dataKey: "deptoSigla", numeric: false, width: 100 },
-    { label: "Descrição", dataKey: "descricao", numeric: false, width: 300 },
-    { label: "Prédio", dataKey: "predioNome", numeric: false, width: 100 },
-    { label: "Área", dataKey: "area", numeric: true, width: 100 },
-    { label: "Latitude", dataKey: "latitude", numeric: true, width: 100 },
-    { label: "Longitude", dataKey: "longitude", numeric: true, width: 100 },
+  const columns: Column<Conferencia>[] = [
+    { label: "Data Solicitação", dataKey: "dataSolicitacao", numeric: false, width: 150 },
+    { label: "Data Realização", dataKey: "dataRealizacao", numeric: false, width: 150 },
+    { label: "Tipo", dataKey: "tipo", numeric: false, width: 150 },
+    { label: "Local", dataKey: "local", numeric: false, width: 150 },
+    { label: "Finalizada", dataKey: "finalizada", numeric: false, width: 150 },
   ];
 
   useEffect(() => {
-      getAllFromCollection("sala").then((data: DocumentData[]) => {
-        const salas: Sala[] = data.map((doc) => ({
+      getAllFromCollection("conferencia").then((data: DocumentData[]) => {
+        const conferencias: Conferencia[] = data.map((doc) => ({
           docId: doc.id,
-          nome: doc.nome,
-          numero: doc.numero,
-          descricao: doc.descricao,
-          deptoSigla: doc.deptoSigla,
-          predioNome: doc.predioNome,
-          area: doc.area,
-          latitude: doc.latitude,
-          longitude: doc.longitude,
-          sigla: doc.sigla,
+          dataSolicitacao: doc.dataSolicitacao,
+          dataLimite: doc.dataLimite,
+          dataRealizacao: doc.dataRealizacao,
+          tipo: doc.tipo,
+          local: doc.local,
+          bensRegistrados: doc.bensRegistrados,
+          finalizada: doc.finalizada,
         }));
-        if (nomePredio) {
-          console.log("Filtrando salas por predio:", nomePredio);
-          setSalasList(salas.filter((sala) => String(sala.predioNome) === String(nomePredio)));
-        } else {
-          setSalasList(salas);
-        }
+        setConferenciasList(conferencias);
       });
-  }, [nomePredio]);
+    }, []);
 
-  const handleOnAdicionarsala = () => {
-    getAllFromCollection("departamento").then((data: DocumentData[]) => {
-      const departamentos: Departamento[] = data.map((doc) => ({
+  const handleOnSolicitarConferencia = () => {
+    getAllFromCollection("sala").then((data: DocumentData[]) => {
+      const salas: Sala[] = data.map((doc) => ({
         docId: doc.id,
         nome: doc.nome,
         descricao: doc.descricao,
@@ -72,79 +57,43 @@ export default function ConferenciasList() {
         predio: doc.predio,
         telefone: doc.telefone,
         email: doc.email,
+        numero: doc.numero,
+        predioNome: doc.predioNome,
+        deptoSigla: doc.deptoSigla,
+        area: doc.area,
+        latitude: doc.latitude,
+        longitude: doc.longitude,
       }));
-      setDepartamentos(departamentos);
+      setSalas(salas);
     });
 
     setSelectedItem(null);
-    setIsEditing(false);
     setDialogOpen(true);
   };
 
-  const handleOnAbrirBens = () => {
-    navigate(`/predio/${selectedRow?.predioNome}/sala/${selectedRow?.sigla}`);
+  const handleOnAbrirConferencia = () => {
+    navigate("");
   }
-
-  const handleOnEdit = (sala: Sala) => {
-    getAllFromCollection("departamento").then((data: DocumentData[]) => {
-      const departamentos: Departamento[] = data.map((doc) => ({
-        docId: doc.id,
-        nome: doc.nome,
-        descricao: doc.descricao,
-        sigla: doc.sigla,
-        predio: doc.predio,
-        telefone: doc.telefone,
-        email: doc.email,
-      }));
-      setDepartamentos(departamentos);
-    });
-    setSelectedItem({ ...sala }); // Cria uma cópia do objeto para edição
-    setIsEditing(true); // Define como edição
-    setDialogOpen(true);
-  };
-
-  const handleOnDelete = (sala: Sala) => {
-    setSelectedItem({ ...sala });
-    setConfirmOpen(true);
-  };
 
   const handleDialogClose = () => {
     setDialogOpen(false);
     setSelectedItem(null);
   };
 
-  const handleSave = (item: Sala) => {
-    if (isEditing) {
-      const sanitizedData = Object.fromEntries(
-        Object.entries(item).filter(([_, value]) => value !== undefined)
-      );
-      setSalasList((prevData) =>
-        prevData.map((sala) => (sala.docId === item.docId ? { ...sala, ...sanitizedData } : sala))
-      );
-      updateItem("sala", sanitizedData.docId, sanitizedData);
-    } else {
-      setSalasList((prevData) => [...prevData, item]);
-      createItem("sala", item);
-    }
+  const handleSave = (item: Conferencia) => {
+    const novaConferencia: Conferencia = {
+      docId: "",
+      dataSolicitacao: new Date(item.dataSolicitacao),
+      dataRealizacao: new Date(item.dataRealizacao),
+      tipo: item.tipo,
+      local: item.local,
+      bensRegistrados: [],
+      finalizada: "NÃO",
+    };
+
+    setConferenciasList((prevData) => [...prevData, novaConferencia]);
+    createItem("conferencia", novaConferencia);
     setDialogOpen(false);
-  };
-
-  const handleConfirmDelete = () => {
-    if (selectedItem) {
-      setSalasList((prevsalas) => prevsalas.filter((d) => d.nome !== selectedItem.nome));
-      deleteItem("sala", selectedItem.nome);
-      setConfirmOpen(false);
-      setSelectedItem(null);
-    }
-  };
-
-  const handleCancelDelete = () => {
-    setConfirmOpen(false);
-    setSelectedItem(null);
-  };
-
-  const handleOnVoltar = () => {
-    navigate(`/predios`);
   };
 
   return (
@@ -152,17 +101,13 @@ export default function ConferenciasList() {
       <div className="header">
         <Typography variant="h4" className="nome-pagina">
           <MeetingRoom className="icon" />
-          Salas
+          Conferências
         </Typography>
-        <Button variant="contained" className="button" onClick={handleOnVoltar}>
-          <ApartmentIcon className="icon" />
-          Voltar
+        <Button variant="contained" className="button" onClick={handleOnSolicitarConferencia}>
+          <AddToQueueIcon className="icon" fontSize="medium" />
+          Solicitar conferência
         </Button>
-        <Button variant="contained" className="button" onClick={handleOnAdicionarsala}>
-          <MeetingRoom className="icon" fontSize="medium" />
-          Adicionar Salas
-        </Button>
-        <Button variant="contained" className="button" onClick={handleOnAbrirBens} disabled={!selectedRow}>
+        <Button variant="contained" className="button" onClick={handleOnAbrirConferencia} disabled={!selectedRow}>
           <ComputerIcon className="icon" />
           Abrir Bens
         </Button>
@@ -170,53 +115,27 @@ export default function ConferenciasList() {
       <div className="body">
         <GenericTable 
           columns={columns}
-          data={salasList}
-          onEdit={handleOnEdit}
-          onDelete={handleOnDelete}
+          data={conferenciasList}
+          onEdit={()=>{}}
+          onDelete={()=>{}}
           onSelectRow={(item) => setSelectedRow(item)} 
-        />
-        <ConfirmDialog
-          title="Confirmar Exclusão"
-          message={`Tem certeza que deseja excluir a sala "${selectedItem?.nome}"?`}
-          onConfirm={handleConfirmDelete}
-          onCancel={handleCancelDelete}
-          open={confirmOpen}
+          disableActionsColumn={true}
         />
         <GenericDialog
           open={dialogOpen}
-          title={isEditing ? "Editar Salas" : "Adicionar Salas"}
+          title={"Solicitar Conferência"}
           item={selectedItem}
-          fields={isEditing? 
-            [ 
-              { label: "Nome", key: "nome", type: "text", disabled: false },
-              { label: "Sigla", key: "sigla", type: "text", disabled: false },
-              { label: "Número", key: "numero", type: "number", disabled: false },
-              { label: "Departamento", key: "deptoSigla", type: "dropdown", disabled: false, options: [
-                ...departamentos.map((d) => ({ label: d.sigla, value: d.sigla }))
-              ], defaultValue: selectedItem?.deptoSigla
-              },
-              { label: "Descrição", key: "descricao", type: "text", disabled: false },
-              { label: "Prédio", key: "predioNome", type: "text", disabled: true },
-              { label: "Área", key: "area", type: "number", disabled: false },
-              { label: "Latitude", key: "latitude", type: "number", disabled: false },
-              { label: "Longitude", key: "longitude", type: "number", disabled: false }, 
+          fields={[
+            { label: "Data Solicitação", type: "date", key: "dataSolicitacao", disabled: true, defaultValue: new Date().toISOString().split("T")[0] },
+            { label: "Tipo", type: "dropdown", key: "tipo", disabled: false, options: [
+              { label: "Ordinária", value: "ordinaria" },
+              { label: "Extraordinária", value: "extraordinaria" }
             ]
-            :
-            [ 
-              { label: "Nome", key: "nome", type: "text", disabled: false },
-              { label: "Sigla", key: "sigla", type: "text", disabled: false },
-              { label: "Número", key: "numero", type: "number", disabled: false },
-              { label: "Departamento", key: "depto", type: "dropdown", disabled: false, options: [
-                ...departamentos.map((d) => ({ label: d.sigla, value: d.sigla }))
-              ]
-              },
-              { label: "Descrição", key: "descricao", type: "text", disabled: false },
-              { label: "Prédio", key: "predioNome", type: "text", defaultValue: nomePredio, disabled: true },
-              { label: "Área", key: "area", type: "number", disabled: false },
-              { label: "Latitude", key: "latitude", type: "number", disabled: false },
-              { label: "Longitude", key: "longitude", type: "number", disabled: false }, 
-            ]
-          }
+            },
+            { label: "Local", type: "dropdown", key: "local", options: [
+              ...salas.map((sala) => ({ label: sala.sigla, value: sala.sigla }))
+            ], disabled: false },
+          ]}
           onClose={handleDialogClose}
           onSave={handleSave}
         />
