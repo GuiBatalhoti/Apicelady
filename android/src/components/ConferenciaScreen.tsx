0f9698edb041a73398839e8e323b5be070101d90
GuiBatalhoti <guibatalhoti@gmail.com>
2024-12-04 18:30:07 -0300

    Android - WIP: Conferência Screen, leitura funcional

--- android/src/components/ConferenciaScreen.tsx
+++ android/src/components/ConferenciaScreen.tsx
@@ -1,19 +1,126 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { useUsbDeviceContext } from '../context/usbDeviceContext';
+import { UsbSerial } from 'react-native-usb-serialport-for-android';
+import { DataTable } from './DataTable';
+import { Bem } from '../types/DataStructures/Bem';
+import { getAllFromCollection } from '../config/firebase';
+import { DocumentData, Timestamp } from 'firebase/firestore';
 
 function ConferenciaScreen({route}: any) {
 
   const navigation = useNavigation();
   const item = route.params.item;
+  const { receivedData, USBDevice } = useUsbDeviceContext() as unknown as { receivedData: string[], USBDevice: UsbSerial|null };
 
-  console.log(item);
+  const [tags, setTags] = useState<string[]>([]);
+  const [isCapturing, setIsCapturing] = useState<boolean>(false);
+  const [bemsList, setBemsList] = useState<Bem[]>([]);
+  const [totalBemsSala, setTotalBemsSala] = useState<number>(0);
+  const [bemsCapturados, setBemsCapturados] = useState<number>(0);
+
+  const checkDuplicate = (tag: string) => {
+    return tags.includes(tag);
+  };
+
+  
+  useEffect(() => {
+    getAllFromCollection("bem").then((data: DocumentData[]) => {
+      const bens: Bem[] = data.map((doc) => ({
+        docId: doc.id,
+        numero: doc.numero,
+        descricao: doc.descricao,
+        data_aquisicao: new Timestamp(doc.data_aquisicao.seconds, doc.data_aquisicao.nanoseconds).toDate(),
+        valor_aquisicao: doc.valor_aquisicao,
+        valor_presente: doc.valor_presente,
+        status: doc.status,
+        condicao_uso: doc.condicao_uso,
+        localizacao: doc.localizacao.map((loc: any) => loc.data ? ({
+          data: new Timestamp(loc.data.seconds, loc.data.nanoseconds).toDate(),
+          atributo: loc.atributo,
+        }) : loc),
+        responsavel: doc.responsavel.map((resp: any) => resp.data ? ({
+          data: new Timestamp(resp.data.seconds, resp.data.nanoseconds).toDate(),
+          atributo: resp.atributo,
+        }) : resp),
+        conferido: doc.conferido ? doc.conferido.map((conf: any) => conf.data ? ({
+          data: new Timestamp(conf.data.seconds, conf.data.nanoseconds).toDate(),
+          atributo: conf.atributo,
+        }) : conf) : [],
+      }
+    ));
+
+    const filteredBens = bens.filter((bem) => bem.localizacao[bem.localizacao.length - 1].atributo === item.sala);
+    setBemsList(filteredBens);
+    setTotalBemsSala(filteredBens.length);
+    });
+  },[]);
+
+  useEffect(() => {
+    if (isCapturing) {
+      let newTags: string[] = [];
+      receivedData.forEach((tag) => {
+        if (!checkDuplicate(tag)) {
+          newTags.push(tag);
+        };
+      });
+      setTags([...tags, ...newTags]);
+    }
+  },[receivedData]);
+
+  const handleOnFinalizarConferirBems = () => {
+    // Implementar
+  }
+
+  const getRowColor = (row: string): string => {
+    const bem = bemsList.find((bem) => bem.numero.toString() === row);
+
+    if (bem?.localizacao !== item.sala) {
+      return "#f8d7da";
+    }
+    if (bem?.localizacao === item.sala) {
+      return "#d4edda";
+    }
+    return "#fff3cd";
+  }
 
   return (
-    <View>
-      <Text>ConferenciaScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>A Sala {item.sala} possui {totalBemsSala} bens alocados. </Text>
+      <Text style={styles.text}>Foram capturados {bemsCapturados} bens. </Text>
+      <View style={styles.buttonRow}>
+        <Button title="Iniciar Captura" onPress={() => setIsCapturing(true)} />
+        <Button title="Finalizar Conferência" onPress={handleOnFinalizarConferirBems} />
+      </View>
+      <DataTable 
+      data={tags}
+      getRowColor={getRowColor} />
     </View>
   );
 }
 
 export default ConferenciaScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+  },
+  text: {
+    fontSize: 18,
+    margin: 10,
+    textAlign: 'center',
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  input: {
+    height: 40,
+    width: 150,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
--- android/src/components/DataTable.tsx
+++ android/src/components/DataTable.tsx
@@ -1,103 +1,96 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, FlatList, StyleSheet, TouchableOpacity, Alert } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Bem } from "../types/DataStructures/Bem";
+import { DataTableProps } from "../types/DataTableProps";
 
-interface DataTableProps {
-    data: string[];
-}
+export function DataTable({ data, getRowColor }: DataTableProps) {
 
-export function DataTable({ data }: DataTableProps) {
-  
-    // Função para lidar com o clique em uma linha
-    function handleRowPress() {
-        // Exibe um alerta com os detalhes, mas pode ser substituído por uma navegação ou outra ação
-        Alert.alert(
-            "Detalhes do Patrimônio",
-            // `Tag: ${item.tag}\nPatrimônio: ${item.patrimonio}\nDescrição: ${item.descricao}`
-        );
-    }
+  function handleRowPress() {
+    Alert.alert(
+      "Detalhes do Patrimônio",
+      // `Tag: ${item.tag}\nPatrimônio: ${item.patrimonio}\nDescrição: ${item.descricao}`
+    );
+  }
 
-    function renderItem({ item }: { item: String }) {
-        return (
-            <TouchableOpacity onPress={() => handleRowPress()}>
-                <View style={styles.row}>
-                    <Text style={styles.cell}>{item}</Text>
-                    {/* <Text style={styles.cell}>{item.patrimonio}</Text> */}
-                </View>
-            </TouchableOpacity>
-        );
-    }
+  function renderItem({ item }: { item: string }) {
+    const rowColor = getRowColor? getRowColor(item) : "#fff";
 
     return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.headerBar}>
-                <Text style={styles.headerBarText}>Patrimônios Encontrados</Text>
-            </View>
-
-            {/* <View style={styles.header}>
-                <Text style={styles.heading}>Tag/Patrimônio</Text>
-            </View> */}
-
-            <FlatList
-                data={data}
-                keyExtractor={(item) => item}
-                renderItem={renderItem}
-                style={styles.flatlist}
-            />
-        </SafeAreaView>
+      <TouchableOpacity onPress={() => handleRowPress()}>
+        <View style={[styles.row, {backgroundColor: rowColor}]}>
+          <Text style={styles.cell}>{item}</Text>
+        </View>
+      </TouchableOpacity>
     );
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.headerBar}>
+        <Text style={styles.headerBarText}>Patrimônios Encontrados</Text>
+      </View>
+
+      <FlatList
+        style={styles.flatlist}
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(row) => row}
+      />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginTop: 30,
-    },
-    headerBar: {
-        flexDirection: "row",
-        justifyContent: "center",
-        paddingVertical: 12,
-        paddingHorizontal: 10,
-        backgroundColor: "#00ceff",
-        borderRadius: 5,
-        elevation: 2,
-    },
-    headerBarText: {
-        fontSize: 16,
-        fontWeight: "bold",
-        color: "black",
-    },
-    header: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        paddingVertical: 10,
-        paddingHorizontal: 10,
-        borderBottomWidth: 1,
-        borderBottomColor: "#f5f5f5",
-    },
-    heading: {
-        flex: 1,
-        fontSize: 14,
-        fontWeight: "bold",
-        textAlign: "center",
-    },
-    flatlist: {
-        flex: 1,
-    },
-    row: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        marginVertical: 8,
-        marginHorizontal: 2,
-        elevation: 1,
-        borderRadius: 5,
-        borderColor: "#fff",
-        padding: 10,
-        backgroundColor: "#fff",
-    },
-    cell: {
-        flex: 1,
-        fontSize: 14,
-        textAlign: "center",
-    },
+  container: {
+    flex: 1,
+    marginTop: 30,
+  },
+  headerBar: {
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    backgroundColor: "#00ceff",
+    borderRadius: 5,
+    elevation: 2,
+  },
+  headerBarText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "black",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#f5f5f5",
+  },
+  heading: {
+    flex: 1,
+    fontSize: 14,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  flatlist: {
+    flex: 1,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 8,
+    marginHorizontal: 2,
+    elevation: 1,
+    borderRadius: 5,
+    borderColor: "#fff",
+    padding: 10,
+    backgroundColor: "#fff",
+  },
+  cell: {
+    flex: 1,
+    fontSize: 14,
+    textAlign: "center",
+  },
 });
